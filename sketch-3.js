7406499dec99f44fee3d17d638c195fe8a0c9ed0
river330 <river330@newschool.edu>
2022-09-13 01:59:15 -0400

    updating the tests to normal

--- sketch-3.js
+++ sketch-3.js
@@ -55,7 +55,7 @@ function draw() {
   for (let i = 0; i < guests.length; i++) {
     for (const p of guests[i].clickHistory) {
       fill(guests[i].color);
-      ellipse(p.x, p.y, 300, 300);
+      ellipse(p.x, p.y, 100, 100);
     }
   }
 
@@ -66,7 +66,7 @@ function draw() {
     ellipse(p.x, p.y, 100, 100);
   }
 
-  fill("orange");
+  fill("black");
   textSize(30);
   text(shared.timer, width / 2, 40);
 }
